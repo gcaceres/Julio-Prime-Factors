45dd8e1cdcb5c2981787844aac195f21f99bc5c2
Gianfranco Caceres <gfcs.798@gmail.com>
2012-04-08 11:03:26 -0500

    Desarrollando Kata 1

--- gcaceres/PrimeFactors.rb
+++ gcaceres/PrimeFactors.rb
@@ -1,8 +1,17 @@
 class PrimeFactors
 
 	def generate(numero)
-		factors = [2,3]
-		factors
-		
+		factores = []
+		index = 0
+        
+		for numFactor in 1..numero 
+			if numero % numFactor ==0
+				factores[index] = numFactor
+				index = index + 1
+
+			end
+		end
+		puts factores
+		factores
 	end
 end
